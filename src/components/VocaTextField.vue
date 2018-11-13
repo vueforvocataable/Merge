ba8b267e6e8edf20d6fb65bdaff612da4912cefd
JungHoon Park <wfr112@naver.com>
2018-11-13 20:17:38 +0900

    Deploy heroku

--- src/components/VocaTextField.vue
+++ src/components/VocaTextField.vue
@@ -35,17 +35,17 @@
       </b-row>
 
       <b-row class="text-center explanation mt-5">
-          <b-col>
-            <b-img width="35" height="35" :src="images.thumbsUp" alt="thumbs-up icon" />
-            <h5>사용이 간편합니다!</h5>
-            <p>텍스트를 입력 후 단어시험지 만들기 버튼을 클릭하고 인쇄버튼을 클릭합니다.</p>
-          </b-col> 
+        <b-col>
+          <b-img width="35" height="35" :src="images.thumbsUp" alt="thumbs-up icon" />
+          <h5>사용이 간편합니다!</h5>
+          <p>텍스트를 입력 후 단어시험지 만들기 버튼을 클릭하고 인쇄버튼을 클릭합니다.</p>
+        </b-col>
 
-           <b-col>
-             <b-img width="35" height="35" :src="images.chrome" alt="chrome icon" />
-            <h5>크롬 최적화</h5>
-            <p>TestPaper은 크롬에 최적화되어있습니다.</p>
-          </b-col>    
+        <b-col>
+          <b-img width="35" height="35" :src="images.chrome" alt="chrome icon" />
+          <h5>크롬 최적화</h5>
+          <p>TestPaper은 크롬에 최적화되어있습니다.</p>
+        </b-col>
       </b-row>
 
       <b-row>
@@ -248,20 +248,24 @@
 
         const blob = new Blob([temp], {
           type: "text/plain;charset=utf-8"
-        });
+        })
 
         saveAs(blob, "Voca.txt");
       },
       //로컬스토리지에 저장한 데이터를 가져옴
       getSavedDataOnLocalStorage: function () {
-        let savedVocas = JSON.parse(localStorage.getItem('savedItems'));
+        try {
+          let savedVocas = JSON.parse(localStorage.getItem('savedItems'))
 
-        if (!savedVocas) return;
+          if (!savedVocas) return
 
-        this.text = "";
-        savedVocas.forEach((item) => {
-          this.text += `${item.english}, ${item.korean}\n`;
-        })
+          this.text = "";
+          savedVocas.forEach((item) => {
+            this.text += `${item.english}, ${item.korean}\n`
+          })
+        } catch (err) {
+          this.text = ""
+        }
       },
       //값을 전달하기전 로컬스토리지에 저장한다
       saveDataOnLocalStorage: function () {
