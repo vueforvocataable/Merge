0cc2ab22bbfe36b8d5a2dfdd4a99ffa97404909c
JungHoon Park <wfr112@naver.com>
2018-11-16 14:18:48 +0900

    Refactoring Explanation

--- src/components/TextFieldPage.vue
+++ src/components/TextFieldPage.vue
@@ -57,22 +57,10 @@
       </b-tooltip>
 
       <!-- 설명 칸 -->
-      <b-row class="text-center explanation mt-5">
-        <b-col>
-          <b-img width="35" height="35" :src="images.thumbsUp" alt="thumbs-up icon" />
-          <h5>사용이 간편합니다!</h5>
-          <p>텍스트를 입력 후 단어시험지 만들기 버튼을 클릭하고 단어를 가리거나 섞은 후 인쇄버튼을 클릭합니다.</p>
-        </b-col>
-
-        <b-col>
-          <b-img width="35" height="35" :src="images.chrome" alt="chrome icon" />
-          <h5>크롬 최적화</h5>
-          <p>TestPaper은 크롬에 최적화되어있습니다.</p>
-        </b-col>
-      </b-row>
+      <explanation class="mt-5"></explanation>
 
       <!-- 다른 사용자가 사용한 단어를 카테고리로 정렬 후 불러옴 -->
-      <expansion-panel></expansion-panel>
+      <expansion-panel class="mt-5"></expansion-panel>
 
 
       <!-- 사용자가 사용한 단어를 불러옴 -->
@@ -97,6 +85,7 @@
   import Preview from './Preview.vue'
   import ExpansionPanel from './ExpansionPanel.vue'
   import Snackbar from './Snackbar.vue'
+  import Explanation from './Explanation.vue'
   import {
     saveAs
   } from '@elastic/filesaver'
@@ -109,6 +98,7 @@
       'preview': Preview,
       'expansionPanel': ExpansionPanel,
       'snackbar': Snackbar,
+      'explanation': Explanation,
     },
     data() {
       return {
@@ -129,8 +119,6 @@
           check: require('../assets/check.png'),
           uncheck: require('../assets/uncheck.png'),
           memo: require('../assets/memo.png'),
-          thumbsUp: require('../assets/thumbs-up.svg'),
-          chrome: require('../assets/chrome.svg'),
           arrow: require('../assets/arrow.svg'),
         },
         buttonGroup: {
